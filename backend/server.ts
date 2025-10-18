a2eeaa85fc212a338fd65d5c6948f76d31ef7144
Shubhankar-Nayak <shubhankarnayak05@gmail.com>
2025-10-18 15:50:09 +0530

    updated cors in server

--- backend/server.ts
+++ backend/server.ts
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:8080'],
+  origin: ['http://localhost:8080', 'https://hws-tau.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
