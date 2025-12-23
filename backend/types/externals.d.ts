f0911aada0f5a9f21e4746ac42ca9f506ac50a40
Shubhankar-Nayak <shubhankarnayak05@gmail.com>
2025-12-23 22:15:58 +0530

    Add global module declarations for JS libraries

---
+++ backend/types/externals.d.ts
@@ -0,0 +1,6 @@
+declare module '@mailchimp/mailchimp_marketing';
+declare module 'md5';
+declare module 'jsonwebtoken';
+declare module 'nodemailer';
+declare module 'cors';
+declare module 'cookie-parser';
